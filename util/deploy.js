faa9df1fc43690469af79d259192f56f994032de
Ian Gilman <ian@iangilman.com>
2020-12-22 15:28:42 -0800

    Added deployment

---
+++ util/deploy.js
@@ -0,0 +1,58 @@
+// SET THE PATH BEFORE RUNNING!
+
+var as = require('async');
+var exec = require('child_process').exec;
+var env = require('./env');
+
+// ----------
+function upload(path) {
+	return new Promise(function(resolve, reject) {
+		var command =
+		'scp -P ' + env.DEPLOY_PORT + ' ' + path + ' ' + env.DEPLOY_DEST + path;
+
+		exec(command, function(err, stdout, stderr) {
+			if (err) {
+				console.log('err', err, stderr);
+				reject();
+			} else {
+				console.log('done', path, stdout);
+				resolve();
+			}
+		});
+	});
+}
+
+// ----------
+var paths = [
+	'js/list.js',
+	'js/main.js',
+	'js/play.js',
+	'lib/jquery-1.11.1.min.js',
+	'lib/less-1.7.5.min.js',
+	'lib/modernizr.custom.js',
+	'lib/normalize.css',
+	'lib/path.js',
+	'lib/path.min.js',
+	'lib/spin.min.js',
+	'lib/underscore-1.5.0.min.js',
+	'lib/velocity.min.js',
+	'lib/zot.js',
+	'lib/zot.subscribable.js',
+	'list/index.html',
+	'play/index.html',
+	'style/list.less',
+	'style/main.less',
+	'style/play.less',
+	'keys.php',
+	'proxy.php'
+];
+
+as.eachSeries(
+	paths,
+	function(path, next) {
+		upload(path).then(next);
+	},
+	function() {
+		console.log('done');
+	}
+);
